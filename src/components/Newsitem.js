fdb5266eb59fdd4031a538bc65d070229f547be5
Amrit Singh <88683562+techmanmotion@users.noreply.github.com>
2022-06-24 18:41:58 +0530

    Update Newsitem.js

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -4,12 +4,15 @@ const Newsitem = ({ item }) => {
     return (
        
             <div className="card news-info" style={{width:"18rem"}}>
-                <img src={item.image} className="card-img-top" alt={item.title} />
+                    
+                <a href={item.url} target="_blank">
+                    <img src={item.image} className="card-img-top" alt={item.title} />
                     <div className="card-body">
                         <h5 className="card-title">{item.title}</h5>
                         <p className="card-text">{item.description}</p>
                         {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
                     </div>
+                </a>
             </div>
         
     )
